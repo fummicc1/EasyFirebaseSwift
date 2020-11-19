832729f7252bf11b8fceae077781a425eaad608f
fummicc1 <fumiya.tennis1@gmail.com>
2020-11-19 18:42:14 +0900

    Initial Commit

---
+++ EasyFirebaseFirestoreSwift/EasyFirebaseFirestoreSwift.h
@@ -0,0 +1,18 @@
+//
+//  EasyFirebaseFirestoreSwift.h
+//  EasyFirebaseFirestoreSwift
+//
+//  Created by Fumiya Tanaka on 2020/11/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EasyFirebaseFirestoreSwift.
+FOUNDATION_EXPORT double EasyFirebaseFirestoreSwiftVersionNumber;
+
+//! Project version string for EasyFirebaseFirestoreSwift.
+FOUNDATION_EXPORT const unsigned char EasyFirebaseFirestoreSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyFirebaseFirestoreSwift/PublicHeader.h>
+
+
