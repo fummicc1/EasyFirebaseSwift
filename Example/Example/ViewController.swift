18fed270daf6c58721b67b1584c2976f893e933d
fummicc1 <fumiya.tennis1@gmail.com>
2021-06-23 10:58:06 +0900

    Update Example

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -5,10 +5,11 @@
 //  Created by Fumiya Tanaka on 2021/05/01.
 //
 
-import UIKit
-import EasyFirebaseSwift
 import FirebaseFirestoreSwift
+import EasyFirebaseSwift
 import FirebaseFirestore
+import Combine
+import UIKit
 
 struct Model: FirestoreModel {
     static var singleIdentifier: String = "model"
@@ -30,13 +31,18 @@ struct Model: FirestoreModel {
 }
 
 class ViewController: UIViewController {
-
+    
     private let client = FirestoreClient()
+    private var cancellables: Set<AnyCancellable> = []
     
     var model = Model(ref: nil, createdAt: nil, updatedAt: nil, message: "Test")
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // MARK: Closure
+        
+        // Snapshot for Collection
         client.listen(
             filter: [],
             order: [],
@@ -51,15 +57,51 @@ class ViewController: UIViewController {
             print(error)
         }
         
+        // Create
         client.create(model) { reference in
             self.model.ref = reference
         } failure: { error in
             print(error)
         }
         
-        model.publisher(for: )
+        // Get single Document
+        client.get(uid: "1234567890") { (model: Model) in
+            print(model.message)
+        } failure: { error in
+            print(error)
+        }
+        
+        // Snapshot for single Document
+        client.listen(uid: "1234567890") { (model: Model) in
+            print(model.message)
+        } failure: { error in
+            print(error)
+        }
+        
+        // MARK: Combine
+        
+        // Create
+        model.publisher(for: .create).sink { error in
+            print(error)
+        } receiveValue: { }
+        .store(in: &cancellables)
+                
+        // Get
+        let ref = Firestore.firestore().collection("models").document("sample")
+        Model.publisher(for: .get(ref: ref)).sink { completion in
+            switch completion {
+            case .failure(let error):
+                print(error)
+            case .finished:
+                break
+            }
+        } receiveValue: { model in
+            print(model.message)
+        }
+        .store(in: &cancellables)
+        
     }
-
+    
     @IBAction func update() {
         model.message = "Update Test"
         client.update(model, success: {  }, failure: { _ in })
