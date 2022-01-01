f488dd3dd96f4a71dac963d65f566756abc9c72c
fummicc1 <fumiya.tennis1@gmail.com>
2022-01-01 12:04:51 +0900

    :construction: Adding Example

---
+++ Example/AuthExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthExample
+//
+//  Created by Fumiya Tanaka on 2022/01/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
