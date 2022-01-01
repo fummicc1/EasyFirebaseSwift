f9135178dea9be19ba5da57687e93b8849d54f6b
fummicc1 <fumiya.tennis1@gmail.com>
2022-01-01 12:04:51 +0900

    :construction: Adding Example

---
+++ Example/FirestoreExample/Model.swift
@@ -0,0 +1,27 @@
+//
+//  Model.swift
+//  Example
+//
+//  Created by Fumiya Tanaka on 2022/01/01.
+//
+
+import Foundation
+import FirebaseFirestore
+import EasyFirebaseSwift
+import FirebaseFirestoreSwift
+
+struct Model: FirestoreModel {
+
+    static var collectionName: String = "models"
+
+    @DocumentID
+    var ref: DocumentReference?
+
+    @ServerTimestamp
+    var createdAt: Timestamp?
+
+    @ServerTimestamp
+    var updatedAt: Timestamp?
+
+    var message: String
+}
