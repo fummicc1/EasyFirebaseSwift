fd8cf01105c351c23b9d6525e27183fbdd3e05e0
fummicc1 <fumiya.tennis1@gmail.com>
2022-01-10 00:13:32 +0900

    :memo: Update Example Project

--- Examples/Practical/BoardApplication/BoardApplication/BoardApplicationApp.swift
+++ Examples/Practical/BoardApplication/BoardApplication/BoardApplicationApp.swift
@@ -1,17 +1,21 @@
-//
-//  BoardApplicationApp.swift
-//  BoardApplication
-//
-//  Created by Fumiya Tanaka on 2022/01/09.
-//
-
 import SwiftUI
+import FirebaseCore
 
 @main
 struct BoardApplicationApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var  appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
---
+++ Examples/Practical/BoardApplication/BoardApplication/Model.swift
@@ -0,0 +1,46 @@
+import Combine
+import Foundation
+import EasyFirebaseSwift
+
+@MainActor
+class Model: ObservableObject {
+
+    @Published var list: [Message] = []
+    @Published var nickName: String = ""
+    @Published var newMessageText: String = ""
+
+    private var cancellables: Set<AnyCancellable> = []
+    private let firestoreClient: FirestoreClient = FirestoreClient()
+
+    init() {
+        let action = FirestoreModelTypeAction<Message>
+            .snapshots(SnapshotInputParameter.Default())
+        Message.multiple(for: action, client: firestoreClient)
+            .sink { _ in } receiveValue: { messages in
+                self.list = messages
+            }
+            .store(in: &cancellables)
+    }
+
+    func create() async {
+
+        if nickName.isEmpty {
+            return
+        }
+
+        if newMessageText.isEmpty {
+            return
+        }
+
+        let newMessage = Message(
+            text: newMessageText,
+            sender: nickName
+        )
+        do {
+            try await firestoreClient.create(newMessage)
+            newMessageText = ""
+        } catch {
+            print(error)
+        }
+    }
+}
