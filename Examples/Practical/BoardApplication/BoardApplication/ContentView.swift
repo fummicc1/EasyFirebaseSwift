c0e6536832156d9edba43d7fa04319a78feaa193
fummicc1 <fumiya.tennis1@gmail.com>
2022-01-09 21:21:19 +0900

    :tada: Add Example application, BoardApplication

---
+++ Examples/Practical/BoardApplication/BoardApplication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BoardApplication
+//
+//  Created by Fumiya Tanaka on 2022/01/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
