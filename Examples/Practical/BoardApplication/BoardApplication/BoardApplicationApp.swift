f2674008d4459b42a4c1a54cbde6117037be860a
fummicc1 <fumiya.tennis1@gmail.com>
2022-01-09 21:21:19 +0900

    :tada: Add Example application, BoardApplication

---
+++ Examples/Practical/BoardApplication/BoardApplication/BoardApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardApplicationApp.swift
+//  BoardApplication
+//
+//  Created by Fumiya Tanaka on 2022/01/09.
+//
+
+import SwiftUI
+
+@main
+struct BoardApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
