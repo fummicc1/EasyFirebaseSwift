5dfd27779d92cbcb00d45609c0b918b2d54b5942
fummicc1 <fumiya.tennis1@gmail.com>
2022-01-16 17:14:23 +0900

    :loud_sound: Add debug log

--- firebase/emulator_setup.sh
+++ firebase/emulator_setup.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+# stop when error
+set -e
+# for debug
+set -x
+
 # check whether firebase has been installed or not.
 if [! type firebase >/dev/null 2>&1]; then
+    # download firebase
     curl -sL https://firebase.tools | bash
 fi
+# setup firebase emulator only for firestore
 firebase login:ci --token "$FIREBASE_TOKEN"
 firebase setup:emulators:firestore
 firebase emulators:start --only firestore
