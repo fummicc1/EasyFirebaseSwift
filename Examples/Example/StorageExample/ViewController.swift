08886a6e43a49cff70e5dbc53d661b1e5d5247f8
fummicc1 <fumiya.tennis1@gmail.com>
2022-03-13 00:28:10 +0900

    add: StorageExample

--- Examples/Example/StorageExample/ViewController.swift
+++ Examples/Example/StorageExample/ViewController.swift
@@ -6,15 +6,103 @@
 //
 
 import UIKit
+import Photos
+import PhotosUI
+import Combine
+import FirebaseStorage
 import EasyFirebaseSwiftStorage
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var imageView: UIImageView!
+
+    var cancellables: Set<AnyCancellable> = []
+    var image: UIImage? {
+        didSet {
+            DispatchQueue.main.async { [weak self] in
+                self?.imageView.image = self?.image
+            }
+        }
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        StorageClient.shared.storage = Storage.storage()
+    }
 
+    @IBAction func didTapImagePickerButton() {
+        if #available(iOS 14, *) {
+            var config = PHPickerConfiguration()
+            config.selectionLimit = 1
+            config.filter = .images
+            config.preferredAssetRepresentationMode = .current
+            let picker = PHPickerViewController(configuration: config)
+            picker.delegate = self
+            present(picker, animated: true, completion: nil)
+        } else {
+            let picker = UIImagePickerController(rootViewController: self)
+            picker.sourceType = .photoLibrary
+            picker.delegate = self
+            present(picker, animated: true, completion: nil)
+        }
+    }
+
+    @IBAction func didTapDoneButton() {
+        guard let image = image else {
+            return
+        }
+        let imageData = image.jpegData(compressionQuality: 1.0)
+        let folder = RootFolder(name: "StorageExample")
+        let metaData = Resource.Metadata(contentType: .jpeg)
+        let resource = Resource(
+            name: "My Photo",
+            folder: folder,
+            metadata: metaData,
+            data: imageData
+        )
+        resource.upload()
+            .sink { task in
+                switch task.status {
+                case .progress(let fractionComplete):
+                    print(fractionComplete)
+                case .success:
+                    break
+                case .fail(let error):
+                    print(error)
+                }
+            }.store(in: &cancellables)
+    }
 }
 
+@available(iOS 14, *)
+extension ViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let result = results.first else {
+            return
+        }
+        if result.itemProvider.canLoadObject(ofClass: UIImage.self) {
+            result.itemProvider.loadObject(
+                ofClass: UIImage.self
+            ) { [weak self] image, error in
+                if let error = error {
+                    print(error)
+                } else {
+                    self?.image = image as? UIImage
+                }
+            }
+        }
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+@available(iOS, introduced: 14, unavailable)
+extension ViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        let image = info[.originalImage] as? UIImage
+        self.image = image
+    }
+}
