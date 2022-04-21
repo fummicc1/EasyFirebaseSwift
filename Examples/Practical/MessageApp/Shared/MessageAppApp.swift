8caff6867b8c7a55454bb41f24f34dffa62512ac
fummicc1 <fumiya.tennis1@gmail.com>
2022-04-21 22:41:41 +0900

    Start creating example app `MessageApp`

---
+++ Examples/Practical/MessageApp/Shared/MessageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageAppApp.swift
+//  Shared
+//
+//  Created by Fumiya Tanaka on 2022/04/21.
+//
+
+import SwiftUI
+
+@main
+struct MessageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
